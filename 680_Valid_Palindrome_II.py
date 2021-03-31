a6a7bc33434772ef27966f02466aa534fb4afaf8
KLTsai <kunger810@gmail.com>
2021-03-31 17:08:08 +0800

    add 680 again

--- 680_Valid_Palindrome_II.py
+++ 680_Valid_Palindrome_II.py
@@ -1,4 +1,19 @@
 
+'''
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+
+Example 2:
+Input: "abca"
+Output: True
+
+Explanation: You could delete the character 'c'.
+'''
+
+
 def validPalindrome(str):
     i = 0        #forward
     j = len(s)-1 #backward
